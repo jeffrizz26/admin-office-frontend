95275e62863465444a1f3975fa9c4a15db90c618
jeffrizz26 <jeffrizz26@gmail.com>
2026-05-21 22:24:55 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -13,7 +13,7 @@ export default function App() {
   const [loading, setLoading] = useState(true);
 
   // BASE URL LAMANG PARA SA TAMANG ROUTING NG VERCEL
-  const BACKEND_URL = 'https://vercel.app';
+  const BACKEND_URL = 'https://admin-office-backend.vercel.app';
   const ADMIN_SECRET_PASSWORD = '1234';
 
   useEffect(() => {
